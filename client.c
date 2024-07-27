f92a177e273c6cfdd8d79e4f9d9f20c1b707c0d7
Mila Soklovacki <msoklova@2-G-2.42heilbronn.de>
2024-07-27 16:33:27 +0200

    checker for pid if its digits

--- client.c
+++ client.c
@@ -6,13 +6,31 @@
 /*   By: msoklova <msoklova@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:30:20 by msoklova          #+#    #+#             */
-/*   Updated: 2024/07/26 18:16:09 by msoklova         ###   ########.fr       */
+/*   Updated: 2024/07/27 16:27:32 by msoklova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <string.h>
 #include <signal.h>
+
+void	end_message(pid_t pid)
+{
+	unsigned char	c;
+	int				i;
+
+	c = '\0';
+	i = 7;
+	while (i >= 0)
+	{
+		if (((c >> i) & 1) == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(200);
+		i--;
+	}
+}
 
 void	send_message(char *str, pid_t pid)
 {
@@ -30,34 +48,41 @@ void	send_message(char *str, pid_t pid)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			usleep(50);
+			usleep(100);
 			i--;
 		}
 	}
-	c = '\0';
-	i = 7;
-	while (i >= 0)
+	end_message(pid);
+}
+
+int	pid_checker(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
 	{
-		if (((c >> i) & 1) == 0)
-			kill(pid, SIGUSR1);
+		if (ft_isdigit(str[i]) == 1)
+			i++;
 		else
-			kill(pid, SIGUSR2);
-		usleep(100);
-		i--;
+			return (0);
 	}
+	return (1);
 }
 
 int	main(int argc, char *argv[])
 {
 	pid_t	pid;
 
-	if (argc == 3)
+	if (argc == 3 && pid_checker(argv[1]))
 	{
 		pid = ft_atoi(argv[1]);
 		send_message(argv[2], pid);
 	}
 	else
 	{
+		if (pid_checker(argv[1]) == 0)
+			write(1, "WRONG PID, DIGITS ONLY!!!", 25);
 		write(1, "\nHow to use: \n", 14);
 		write(1, "	./client <PID> <string>\n", 25);
 	}
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: msoklova <msoklova@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:30:25 by msoklova          #+#    #+#             */
-/*   Updated: 2024/07/26 18:11:51 by msoklova         ###   ########.fr       */
+/*   Updated: 2024/07/27 15:13:38 by msoklova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,107 @@
 #include <string.h>
 #include <signal.h>
 
+//void	decrypt_sig(int sig)
+//{
+//	static unsigned char	byte = 0;
+//	static int				bit = 0;
+//	static char				*msg = NULL;
+//	static int				i = 0;
+//	static int				msg_len = 0;
+//	static int				buff = 1000;
+//	char					*new_msg;
+
+//	if (msg == NULL)
+//	{
+//		msg = (char *)malloc(buff);
+//		if (msg == NULL)
+//			return ;
+//		msg[0] = '\0';
+//	}
+//	if (sig == SIGUSR1)
+//		byte = (byte << 1);
+//	else if (sig == SIGUSR2)
+//		byte = (byte << 1) | 1;
+//	bit++;
+//	if (bit == 8)
+//	{
+//		if (i >= buff - 1)
+//		{
+//			buff *= 2;
+//			new_msg = (char *)malloc(buff);
+//			if (new_msg == NULL)
+//				return ;
+//			ft_memcpy(new_msg, msg, i);
+//			free(msg);
+//			msg = new_msg;
+//		}
+//		if (byte == '\0')
+//		{
+//			write(1, msg, i);
+//			write(1, "\n", 1);
+//			free(msg);
+//			msg = NULL;
+//			i = 0;
+//			msg_len = 0;
+//			buff = 1000;
+//		}
+//		else
+//		{
+//			msg[i++] = byte;
+//			msg[i] = '\0';
+//		}
+//		byte = 0;
+//		bit = 0;
+//	}
+//}
+
+void	process_signal(int sig, unsigned char *byte, int *bit)
+{
+	if (sig == SIGUSR1)
+		*byte = (*byte << 1);
+	else if (sig == SIGUSR2)
+		*byte = (*byte << 1) | 1;
+	(*bit)++;
+}
+
+void	new_buff(char **msg, int *buff, int i)
+{
+	char	*new_msg;
+
+	*buff *= 2;
+	new_msg = (char *)malloc(*buff);
+	if (new_msg == NULL)
+		return ;
+	ft_memcpy(new_msg, *msg, i);
+	free(*msg);
+	*msg = new_msg;
+}
+
+void	process_byte(char byte, char **msg, int *i, int *buff)
+{
+	if (byte == '\0')
+	{
+		write(1, *msg, *i);
+		write(1, "\n", 1);
+		free(*msg);
+		*msg = NULL;
+		*i = 0;
+		*buff = 1000;
+	}
+	else
+	{
+		(*msg)[(*i)++] = byte;
+		(*msg)[*i] = '\0';
+	}
+}
+
 void	decrypt_sig(int sig)
 {
 	static unsigned char	byte = 0;
 	static int				bit = 0;
 	static char				*msg = NULL;
 	static int				i = 0;
-	static int				msg_len = 0;
 	static int				buff = 1000;
-	char					*new_msg;
 
 	if (msg == NULL)
 	{
@@ -31,38 +123,12 @@ void	decrypt_sig(int sig)
 			return ;
 		msg[0] = '\0';
 	}
-	if (sig == SIGUSR1)
-		byte = (byte << 1);
-	else if (sig == SIGUSR2)
-		byte = (byte << 1) | 1;
-	bit++;
+	process_signal(sig, &byte, &bit);
 	if (bit == 8)
 	{
-		if (i > buff - 1)
-		{
-			buff *= 2;
-			new_msg = (char *)malloc(buff);
-			if (new_msg == NULL)
-				return ;
-			ft_memcpy(new_msg, msg, i);
-			free(msg);
-			msg = new_msg;
-		}
-		if (byte == '\0')
-		{
-			write(1, msg, i);
-			write(1, "\n", 1);
-			free(msg);
-			msg = NULL;
-			i = 0;
-			msg_len = 0;
-			buff = 1000;
-		}
-		else
-		{
-			msg[i++] = byte;
-			msg[i] = '\0';
-		}
+		if (i >= buff - 1)
+			new_buff(&msg, &buff, i);
+		process_byte(byte, &msg, &i, &buff);
 		byte = 0;
 		bit = 0;
 	}
